db8c538b1ce7b644f0da13689bcc2c4351d570e1
shankar23-hub <148693429+shankar23-hub@users.noreply.github.com>
2026-05-09 21:48:54 +0530

    Add Vercel Speed Insights

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import App from './App.jsx'
 import './styles/global.css'
 
